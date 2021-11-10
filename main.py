bea655204c1c7ce3915ae99d18f73950e167ec30
aleksandrgrek <goldoner@gmail.com>
2021-11-10 10:00:37 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import cv2
+
+FACE_CASCADE = cv2.CascadeClassifier('resources/face-detection-config.xml')
+
+def mark_faces_on_image(path_to_image):
+    img = cv2.imread(path_to_image)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect faces
+    faces = FACE_CASCADE.detectMultiScale(gray, 1.1, 4)
+    # Draw rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    # Display the output
+    cv2.imshow('img', img)
+    cv2.waitKey()
+
+
+def detect_faces_on_camera():
+    # To capture video from webcam.
+    cap = cv2.VideoCapture(0)
+    # To use a video file as input
+    # cap = cv2.VideoCapture('filename.mp4')
+
+    while True:
+        # Read the frame
+        _, img = cap.read()
+        # Convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Detect the faces
+        faces = FACE_CASCADE.detectMultiScale(gray, 1.1, 4)
+        # Draw the rectangle around each face
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        # Display
+        cv2.imshow('img', img)
+        # Stop if escape key is pressed
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+    # Release the VideoCapture object
+    cap.release()
+
+
+mark_faces_on_image('resources/picutres/faces-in-a-crowd.jpeg')
+#detect_faces_on_camera()
